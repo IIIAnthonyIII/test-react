edf86e4787a94ddb7eec470292bbc677c8df736e
IIIAnthonyIII <aadvl382016@outlook.com>
2024-06-26 17:03:05 -0500

    iconos

--- src/components/Paciente/Paciente.jsx
+++ src/components/Paciente/Paciente.jsx
@@ -2,47 +2,69 @@ import "./Paciente.css";
 import React, { useState } from "react";
 import { Space, Table, Tag, Button, notification } from "antd";
 import EditPaciente from "./Edit/EditPaciente";
+import { DeleteOutlined, FormOutlined } from "@ant-design/icons";
 
 const Paciente = () => {
   let data = [];
   let columns = [];
   let filter = [];
+  const [tableParams, setTableParams] = useState({
+    pagination: {
+      current: 1,
+      pageSize: 5,
+      pageSizeOptions: [5, 10, 20, 50, 100],
+      showQuickJumper: true,
+      position: ["bottomRight"]
+    },
+  });
 
   //Cargar data
-  data = [
-    {
-      id: "1",
+  // data = [
+  //   {
+  //     id: "1",
+  //     name: "John Brown",
+  //     chinese: 98,
+  //     math: 60,
+  //     english: 22,
+  //     tags: ["nice", "developer"],
+  //   },
+  //   {
+  //     id: "2",
+  //     name: "Jim Green",
+  //     chinese: 98,
+  //     math: 60,
+  //     english: 40,
+  //     tags: ["loser"],
+  //   },
+  //   {
+  //     id: "3",
+  //     name: "Joe Black",
+  //     chinese: 98,
+  //     math: 90,
+  //     english: 10,
+  //     tags: ["cool", "teacher"],
+  //   },
+  //   {
+  //     id: "4",
+  //     name: "Jim Red",
+  //     chinese: 88,
+  //     math: 99,
+  //     english: 89,
+  //     tags: ["nice", "developer"],
+  //   },
+  // ];
+
+  //Data de prueba
+  for (let i = 0; i < 100; i++) {
+    data.push({
+      id: i,
       name: "John Brown",
       chinese: 98,
       math: 60,
       english: 22,
       tags: ["nice", "developer"],
-    },
-    {
-      id: "2",
-      name: "Jim Green",
-      chinese: 98,
-      math: 60,
-      english: 40,
-      tags: ["loser"],
-    },
-    {
-      id: "3",
-      name: "Joe Black",
-      chinese: 98,
-      math: 90,
-      english: 10,
-      tags: ["cool", "teacher"],
-    },
-    {
-      id: "4",
-      name: "Jim Red",
-      chinese: 88,
-      math: 99,
-      english: 89,
-      tags: ["nice", "developer"],
-    },
-  ];
+    });
+  }
 
   //Llenar filtros
   data.forEach(element => {
@@ -120,118 +142,27 @@ const Paciente = () => {
       align: "center",
       render: (_, record) => (
         <Space size="middle">
-          <Button onClick={() => showEditModal(record)}>
-            Editar
+          <Button className="actions" onClick={() => showEditModal(record)}>
+            <FormOutlined />
           </Button>
-          <Button>Eliminar</Button>
+          <Button className="actions">
+            <DeleteOutlined />
+          </Button>
         </Space>
       ),
     },
   ];
-  
-  //Data de prueba
-  // for (let i = 0; i < 100; i++) {
-  //   data.push({
-  //     key: i,
-  //     name: 'John Brown',
-  //     age: i + 1,
-  //     street: 'Lake Park',
-  //     building: 'C',
-  //     number: 2035,
-  //     companyAddress: 'Lake Street 42',
-  //     companyName: 'SoftLake Co',
-  //     gender: 'M',
-  //   });
-  // }
-
-  // columns = [
-  //   {
-  //     title: 'Name',
-  //     dataIndex: 'name',
-  //     key: 'name',
-  //     width: 100,
-  //     fixed: 'left',
-  //     filters: [
-  //       {
-  //         text: 'Joe',
-  //         value: 'Joe',
-  //       },
-  //       {
-  //         text: 'John',
-  //         value: 'John',
-  //       },
-  //     ],
-  //     onFilter: (value, record) => record.name.indexOf(value) === 0,
-  //   },
-  //   {
-  //     title: 'Other',
-  //     children: [
-  //       {
-  //         title: 'Age',
-  //         dataIndex: 'age',
-  //         key: 'age',
-  //         width: 150,
-  //         sorter: (a, b) => a.age - b.age,
-  //       },
-  //       {
-  //         title: 'Address',
-  //         children: [
-  //           {
-  //             title: 'Street',
-  //             dataIndex: 'street',
-  //             key: 'street',
-  //             width: 150,
-  //           },
-  //           {
-  //             title: 'Block',
-  //             children: [
-  //               {
-  //                 title: 'Building',
-  //                 dataIndex: 'building',
-  //                 key: 'building',
-  //                 width: 100,
-  //               },
-  //               {
-  //                 title: 'Door No.',
-  //                 dataIndex: 'number',
-  //                 key: 'number',
-  //                 width: 100,
-  //               },
-  //             ],
-  //           },
-  //         ],
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     title: 'Company',
-  //     children: [
-  //       {
-  //         title: 'Company Address',
-  //         dataIndex: 'companyAddress',
-  //         key: 'companyAddress',
-  //         width: 200,
-  //       },
-  //       {
-  //         title: 'Company Name',
-  //         dataIndex: 'companyName',
-  //         key: 'companyName',
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     title: 'Gender',
-  //     dataIndex: 'gender',
-  //     key: 'gender',
-  //     width: 80,
-  //     fixed: 'right',
-  //   },
-  // ];
 
   const handleTableChange = (pagination, filters, sorter) => {
     console.log(pagination)
     console.log(filters)
     console.log(sorter)
+    setTableParams({
+      pagination,
+      filters,
+      sortOrder: Array.isArray(sorter) ? undefined : sorter.order,
+      sortField: Array.isArray(sorter) ? undefined : sorter.field,
+    });
   };
 
   //Modal
@@ -253,17 +184,17 @@ const Paciente = () => {
 
   //Notification
   const [api, contextHolder] = notification.useNotification();
-  const openNotification = (placement) => {
-    if (placement == 'Exito'){
+  const openNotification = (title) => {
+    if (title == 'Exito'){
       api.success({
-        message: `${placement}`,
+        message: `${title}`,
         description: "Se ha editado correctamente",
         showProgress: true,
         placement: "topRight",
       });
     }else{
       api.error({
-        message: `${placement}`,
+        message: `${title}`,
         description: "Ha habido un error, no se ha podido editar!!",
         showProgress: true,
         placement: "topRight",
@@ -279,10 +210,7 @@ const Paciente = () => {
         columns={columns}
         dataSource={data}
         rowKey={"id"}
-        pagination={{
-          pageSize: 2,
-          position: ["bottomRight"]
-        }}
+        pagination={tableParams.pagination}
         onChange={handleTableChange} />
       <EditPaciente
         isModalOpen={isModalOpen}
